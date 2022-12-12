9c6e5fa941e73d25689ffe70dd68ee632d61a519
Tomo-ja <tomonao.hj@gmail.com>
2022-12-11 17:55:06 -0800

    feat: create navigation system with new url dashboard

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,23 +12,14 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = []
-  selectedHero?: Hero;
 
-
-
-  constructor(private heroService: HeroService, private messageService: MessageService) {}
-
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes()
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
 }
